5262fafd4c20b9d88a70fa91671ee54a3f026819
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-06-25 16:23:33 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (43.04%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int last=digits.length-1; last>=0; last--){
+            if(digits[last]!=9){
+                digits[last]++;
+                return digits;
+            }
+            else
+                digits[last]=0;
+        }
+        int[] result= new int[digits.length+1];
+        result[0]=1;
+        for(int i=1;i<result.length;i++)
+            result[i]=0;
+        return result;
+    }
+}
