e2b704b5a4ef6b6ae5e118431b764efd9764450e
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-06-26 13:15:49 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (94.73%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        for(int i=2; i<n;i++){
+            cost[i]+=(cost[i-1]<cost[i-2])?cost[i-1]:cost[i-2];
+        }
+        return (cost[n-1]<cost[n-2])?cost[n-1]:cost[n-2];
+    }
+}
