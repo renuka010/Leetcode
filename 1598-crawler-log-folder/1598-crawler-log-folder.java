8a6bb4d0b1616aca570e98ad76d171e15c606c4f
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-01 00:10:28 +0530

    Time: 2 ms (53.41%), Space: 42.7 MB (42.16%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minOperations(String[] logs) {
         int x=0;
-        for(String s: logs){
-            if(s.contains("../"))
+        for(int i=0; i<logs.length; i++){
+            if(logs[i].contains("../"))
                 x=Math.max(0,x-1);
-            else if(s.contains("./"))
+            else if(logs[i].contains("./"))
                 continue;
             else
                 x++;
