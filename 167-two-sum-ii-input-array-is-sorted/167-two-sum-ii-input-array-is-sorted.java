fea1c75affd58a771aba4fce57f92053d968bd3b
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-07 11:40:43 +0530

    Time: 3 ms (36.04%), Space: 50.5 MB (18.51%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,24 +1,9 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int start=0, end=numbers.length-1;
-        int x=target-numbers[0];
-        //special case
         if(numbers[0]+numbers[1]==target)
             return new int[]{1,2};
         
-        //binary search to find the max element that can be used to make target
-        /*while(start<end){
-            int mid=(start+end)/2;
-            if(numbers[mid]<x)
-                start=mid+1;
-            else
-                end=mid;
-            
-            if(start==end)
-                break;
-        }*/
-        
-        //finding sum using two pointers
         while(start<end){
             if(numbers[start]+numbers[end]==target)
                 break;
