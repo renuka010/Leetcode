afda41f8b8b9a554326bf02f57b0275235abb4c4
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-11 12:59:03 +0530

    Time: 7 ms (74.22%), Space: 42 MB (75.24%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -3,15 +3,15 @@ class Solution {
         ArrayList<String> res=new ArrayList<>();
         int i=0, n=nums.length;
         while(i<n){
-            StringBuilder s=new StringBuilder();
-            s.append(Integer.toString(nums[i]));
+            int s=nums[i];
             int j=i;
             while(i+1<n && nums[i+1]-1==nums[i]){
                 i++;
             }
-            if(i!=j)
-                s.append("->"+Integer.toString(nums[i]));
-            res.add(s.toString());
+            if(s!=nums[i])
+                res.add(s+"->"+nums[i]);
+            else
+                res.add(Integer.toString(s));
             i++;
         }
         return res;
