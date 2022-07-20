47e1fdf8e9dcdb333c1ab2264fe9e4999a7d5c06
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-20 21:21:02 +0530

    Time: 3 ms (36.68%), Space: 41.5 MB (43.68%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -6,11 +6,9 @@ class Solution {
             if(mid>x/mid)
                 right=mid-1;
             else{
-                if((mid+1)>x/(mid+1))
-                    return mid;
                 left=mid+1;
             }
         }
-        return 0;
+        return right;
     }
 }
