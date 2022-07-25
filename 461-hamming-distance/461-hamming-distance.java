a150a19b9cc3328688acf91e31886adeec1eced1
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-26 00:10:58 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (37.43%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -1,9 +1,10 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int count=0;
-        for(int i=31; i>=0; i--){
-            if(((x>>i)&1)!=((y>>i)&1))
+        int xor=x^y, count=0;
+        while(xor!=0){
+            if((xor&1)==1)
                 count++;
+            xor>>=1;
         }
         return count;
     }
