12a9c7c02cf27f6f3934bd7104f11e953d4bc905
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-29 18:32:20 +0530

    Time: 5 ms (69.48%), Space: 62.9 MB (25.14%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean visited[]=new boolean[arr.length];
+        return canU(visited,arr,start);
+    }
+    public boolean canU(boolean[] visited, int[] arr, int start){
+        boolean ans=false;
+        if(start>=0 && start<arr.length && !visited[start]){
+            if(arr[start]==0)
+                return true;
+            visited[start]=true;
+            ans= canU(visited,arr,start-arr[start])||canU(visited,arr,start+arr[start]);
+            visited[start]=false;
+            return ans;
+        }
+        return ans;
+    }
+}
