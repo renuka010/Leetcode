09d9bc731dcd219be310b208620bab6f08c52c27
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-29 14:12:23 +0530

    Time: 3 ms (66.18%), Space: 67.6 MB (61.27%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0; i<nums.length; i++){
+            if(i>max)
+                return false;
+            if(i+nums[i]>max)
+                max=nums[i]+i;
+        }
+        return true;
+    }
+}
