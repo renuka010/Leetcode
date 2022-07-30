d22c2eb85e070c08f219a180b499dc5977b902a2
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-07-30 18:23:54 +0530

    Time: 30 ms (77.64%), Space: 89.6 MB (48.78%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] arr1=new int[26];
+        int i;
+        List<String> res=new ArrayList<>();
+        
+        for(String s:words2){
+            int[] arr2=new int[26];
+            for(i=0; i<s.length(); i++)
+                arr2[s.charAt(i)-'a']++;
+            for(i=0; i<26; i++){
+                if(arr2[i]>arr1[i])
+                    arr1[i]=arr2[i];
+            }
+        }
+        
+        for(String s:words1){
+            boolean issubset=true;
+            int[] arr2=new int[26];
+            for(i=0; i<s.length(); i++)
+                arr2[s.charAt(i)-'a']++;
+            for(i=0; i<26; i++){
+                if(arr1[i]!=0 && arr1[i]>arr2[i]){
+                    issubset=false;
+                    break;
+                }
+            }
+            if(issubset)
+                res.add(s);
+        }
+        return res;
+    }
+}
