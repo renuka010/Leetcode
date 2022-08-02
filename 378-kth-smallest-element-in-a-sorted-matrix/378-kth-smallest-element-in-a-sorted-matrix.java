1fc12d1269f5f6e75fe6548a5c743a198c215762
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-08-02 11:55:26 +0530

    Time: 21 ms (54.59%), Space: 57.9 MB (12.68%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,16 +1,27 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        Queue<Integer> pq=new PriorityQueue<>();
-        for(int i=0; i<matrix.length ;i++){
-            for(int j=0; j<matrix.length; j++){
-                pq.offer(matrix[i][j]);
-            }
+        Queue<Tuple> pq=new PriorityQueue<>();
+        for(int i=0; i<matrix.length ;i++)
+            pq.offer(new Tuple(0,i,matrix[0][i]));
+        for(int i=k; k>1; k--){
+            Tuple t=pq.poll();
+            if(t.x==matrix.length-1)
+                continue;
+            pq.offer(new Tuple(t.x+1,t.y,matrix[t.x+1][t.y]));
         }
-        int x=0;
-        while(k!=0){
-            x=pq.poll();
-            k--;
-        }
-        return x;
+        return pq.poll().val;
+    }
+}
+
+class Tuple implements Comparable<Tuple>{
+    int x, y, val;
+    public Tuple(int i,int j, int val){
+        this.x=i;
+        this.y=j;
+        this.val=val;
+    }
+    @Override
+    public int compareTo(Tuple that){
+        return this.val-that.val;
     }
 }
