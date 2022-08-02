55f4f7200baed458504ebf93de1e2e42959f0e75
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-08-02 19:11:04 +0530

    Time: 2 ms (59.38%), Space: 49.3 MB (20.62%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] h) {
+        int[] minmax=new int[h.length];
+        int max=0, sum=0, res=0;
+        for(int i=0; i<h.length; i++){
+            minmax[i]=max;
+            if(h[i]>max)
+                max=h[i];
+        }
+        max=0;
+        for(int i=h.length-1; i>=0; i--){
+            minmax[i]=Math.min(minmax[i],max)-h[i];
+            if(h[i]>max)
+                max=h[i];
+            if(minmax[i]>0)
+                sum+=minmax[i];
+            else
+                res=Math.max(sum,res);
+        }
+        return Math.max(sum,res);
+    }
+}
