77838080b62572639e9ae096aef96464096be589
renuka010 <72569696+renuka010@users.noreply.github.com>
2022-08-05 23:29:07 +0530

    Time: 7 ms (80.50%), Space: 43.4 MB (48.84%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,24 +1,41 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] arr=new int[26];
-        int i, l1=s1.length(), l2=s2.length();
+        int[] arr2=new int[26];
+        int i, l1=s1.length(), l2=s2.length(), match=0;;
+        if(l1>l2)
+            return false;
         for(i=0; i<l1; i++)
             arr[s1.charAt(i)-'a']++;
-        for(i=0; i<=l2-l1; i++){
-            if(perm(arr,s2,i,i+l1-1))
+        for(i=0; i<l1; i++)
+            arr2[s2.charAt(i)-'a']++;
+        for(i=0; i<26; i++){
+            if(arr[i]==arr2[i])
+                match++;
+        }
+        if(match==26)
+            return true;
+        
+        for(i=1; i<=l2-l1; i++){
+            int c1=s2.charAt(i-1)-'a';
+            int c2=s2.charAt(i+l1-1)-'a';
+            
+            if(arr[c1]==arr2[c1])
+                match--;
+            if(arr[c2]==arr2[c2])
+                match--;
+
+            arr2[c1]--;
+            arr2[c2]++;
+            
+            if(arr[c2]==arr2[c2])
+                match++;
+            if(arr[c1]==arr2[c1])
+                match++;
+            
+            if(match==26)
                 return true;
         }
         return false;
-    }
-    public boolean perm(int[] arr, String s2, int start, int end){
-        int[] arr2=arr.clone();
-        for(int i=start; i<=end; i++){
-            arr2[s2.charAt(i)-'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(arr2[i]!=0)
-                return false;
-        }
-        return true;
     }
 }
