e5a2fb73f404721f7328cf3fce811acab2efd3f5
Renuka Bhaskaran <renukabhaskaran1395@gmail.com>
2022-11-22 11:35:26 +0530

    comitted

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution { //comment
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head=l1;
         int carry=0;
