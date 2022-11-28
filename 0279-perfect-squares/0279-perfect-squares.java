ffcfd42ea1d7a2ccc4d530ba90155aa1bd5e6c75
Renuka B <72569696+renuka010@users.noreply.github.com>
2022-11-28 14:04:18 +0530

    Time: 37 ms (76.96%), Space: 41.4 MB (88.83%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,n+1);
+        dp[0]=0;
+        for(int i=0; i<=n; i++){
+            for(int j=1; j*j<=i; j++){
+                dp[i]=Math.min(dp[i],dp[i-j*j]+1);
+            }
+        }
+        return dp[n];
+    }
+}
