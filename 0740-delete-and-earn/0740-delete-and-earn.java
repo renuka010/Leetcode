319ad3ef3c22b368fbbd1cfa6bb12a76fdace3c9
Renuka B <72569696+renuka010@users.noreply.github.com>
2022-11-28 12:31:37 +0530

    Time: 37 ms (16.75%), Space: 47.6 MB (52.39%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,28 +1,24 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         Arrays.sort(nums);
-        int prev=0, i=0;
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int x:nums){
-            if(prev!=x){
-                nums[i++]=x;
-            }
-            prev=x;
             map.put(x,map.getOrDefault(x,0)+1);
         }
-        if(i==1)
-            return map.get(nums[0])*nums[0];
-        
-        int[] res=new int[i];
-        for(int j=0; j<i; j++){
-            if(j !=0 && nums[j]==nums[j-1]+1){
-                res[j]=Math.max(res[j-1],((j==1)?0:res[j-2])+map.get(nums[j])*nums[j]);
+        int prev=map.get(nums[0])*nums[0], prev2=0,curr;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==nums[i-1])
+                continue;
+            if(map.containsKey(nums[i]-1)){
+                curr=Math.max(prev,prev2+map.get(nums[i])*nums[i]);
             }
             else{
-                res[j]=((j==0)?0:res[j-1])+map.get(nums[j])*nums[j];
+                curr=prev+map.get(nums[i])*nums[i];
             }
+            prev2=prev;
+            prev=curr;
         }
         
-        return Math.max(res[i-1],res[i-2]);
+        return prev;
     }
 }
