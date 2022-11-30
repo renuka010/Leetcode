ce9b9054de7521d8efa2230ed2e395ccbcb9fad9
Renuka B <72569696+renuka010@users.noreply.github.com>
2022-11-30 18:01:07 +0530

    Time: 17 ms (99.87%), Space: 50.6 MB (91.42%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,37 +1,12 @@
 class Solution {
     public int minSwaps(String s) {
-        int lo=0,lc=0,ro=0,rc=0;
-        int l=0, r=s.length()-1, swap=0;
-        while(l<r){
-            if(s.charAt(l)=='[')
-                lo++;
-            else
-                lc++;
-            if(s.charAt(r)=='[')
-                ro++;
-            else
-                rc++;
-            if(lo<lc && ro>rc){
-                swap++;
-                lc--;
-                lo++;
-                ro--;
-                rc++;
-                l++;
-                r--;
-                continue;
-            }
-            if(lo<lc){
-                lc--;
-            }
-            else
-                l++;
-            if(ro>rc){
-                ro--;
-            }
-            else
-                r--;
+        int badops=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='[')
+                badops++;
+            else if(badops!=0)
+                badops--;
         }
-        return swap;
+        return (badops+1)/2;
     }
 }
