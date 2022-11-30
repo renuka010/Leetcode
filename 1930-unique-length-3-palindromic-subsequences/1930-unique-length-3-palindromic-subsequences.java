e3b955f6da7870744e380f721cd0d0e606bb8df4
Renuka B <72569696+renuka010@users.noreply.github.com>
2022-11-30 15:16:11 +0530

    Time: 2245 ms (7.08%), Space: 44.9 MB (83.65%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,30 +1,21 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
         int n=s.length();
-        int[][] left=new int[n][26];
-        int[][] right=new int[n][26];
+        Set<Integer> left=new HashSet<>();
+        int[] right=new int[26];
+        left.add(s.charAt(0)-'a');
+        for(int i=2; i<s.length(); i++){
+            right[s.charAt(i)-'a']++;
+        }
+        
         Set<String> set=new HashSet<>();
-        left[0][s.charAt(0)-'a']++;
-        for(int i=1; i<n; i++){
-            left[i][s.charAt(i)-'a']++;
+        for(int i=1; i<s.length()-1; i++){
             for(int j=0; j<26; j++){
-                left[i][j]+=left[i-1][j];
+                if(right[j]!=0 && left.contains(j))
+                    set.add(s.charAt(i)+Integer.toString(j));
             }
-        }
-        right[n-1][s.charAt(n-1)-'a']++;
-        for(int i=n-2; i>=0; i--){
-            right[i][s.charAt(i)-'a']++;
-            for(int j=0; j<26; j++){
-                right[i][j]+=right[i+1][j];
-            }
-        }
-        for(int i=1; i<n-1; i++){
-            for(int j=0; j<26; j++){
-                if(left[i-1][j]!=0 && right[i+1][j]!=0){
-                    String s1=Integer.toString(j)+s.charAt(i);
-                    set.add(s1);
-                }
-            }
+            left.add(s.charAt(i)-'a');
+            right[s.charAt(i+1)-'a']--;
         }
         return set.size();
     }
